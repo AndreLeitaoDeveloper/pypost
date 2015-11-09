0e67ed59a925bdfc2383d3be32807f94a1d0abfd
Andre Leitao <andre.leitao@zap.co.ao>
2015-11-09 16:29:25 +0000

    add - core files

---
+++ index.py
@@ -0,0 +1,12 @@
+import urllib2
+import random
+import names
+
+for i in range(18990):
+
+	x = random.randint(0,len(names.names))
+	y = random.randint(0,len(names.names))
+	req = "http://www.guerradosbairros.pt/vote.php?email="+names.names[y].lower() + names.names[x].lower()+str(x)+str(y)+"@gmail.com&zone_id=EX"
+	urllib2.urlopen(req)
+	if i%100 == 0:
+		print i
