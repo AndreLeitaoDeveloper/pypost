6c1fd0af10230a10460a30d428ada7d197d96ee9
Andre Leitao <andre.leitao@zap.co.ao>
2015-11-09 17:28:45 +0000

    fix - count len index not found

--- index.py
+++ index.py
@@ -4,8 +4,8 @@ import names
 
 for i in range(18990):
 
-	x = random.randint(0,len(names.names))
-	y = random.randint(0,len(names.names))
+	x = random.randint(0,len(names.names)-1)
+	y = random.randint(0,len(names.names)-1)
 	req = "http://www.guerradosbairros.pt/vote.php?email="+names.names[y].lower() + names.names[x].lower()+str(x)+str(y)+"@gmail.com&zone_id=EX"
 	urllib2.urlopen(req)
 	if i%100 == 0:
