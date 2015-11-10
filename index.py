77f37c280c25d34ec0b85cc090c83ca1216fc386
Andre Leitao <andre.leitao@zap.co.ao>
2015-11-10 10:26:42 +0000

    ref - random sleep alvaro

--- index.py
+++ index.py
@@ -1,12 +1,19 @@
 import urllib2
 import random
 import names
+import time
 
-for i in range(18990):
+for i in range(20000):
 
+	t = random.randint(0,3)
 	x = random.randint(0,len(names.names)-1)
 	y = random.randint(0,len(names.names)-1)
 	req = "http://www.guerradosbairros.pt/vote.php?email="+names.names[y].lower() + names.names[x].lower()+str(x)+str(y)+"@gmail.com&zone_id=EX"
-	urllib2.urlopen(req)
-	if i%100 == 0:
-		print i
+	try:
+		time.sleep(t)
+		urllib2.urlopen(req)
+		print i,t
+		#if i%100 == 0:
+			#print i
+	except:
+		print 'O Alvaro nao desiste!'
